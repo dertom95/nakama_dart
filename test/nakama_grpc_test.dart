e4be52dc893cc4ad0cc6e65d54d375a1cde30f06
Thomas Trocha <thomas.trocha@gmail.com>
2020-10-16 23:54:24 +0200

    remove files to get it to resolve errors

--- test/nakama_grpc_test.dart
+++ test/nakama_grpc_test.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:grpc/grpc.dart';
+import 'package:nakama_client/src/generated/proto/api.pb.dart';
 import 'package:nakama_client/src/generated/proto/apigrpc.pbgrpc.dart';
 import 'package:nakama_client/src/generated/proto/google/protobuf/empty.pb.dart';
 
@@ -11,7 +12,7 @@ void main() {
   group("", () {
     test("Test grpc connection", () async {
       // todo test code here
-      String nakamaHost = "134.209.168.18";
+      String nakamaHost = "127.0.0.1";
       int nakamaPort = 7349;
       String defaultServerKey = "defaultkey:";
       ClientChannel channel = new ClientChannel(
@@ -24,10 +25,31 @@ void main() {
       NakamaClient client = new NakamaClient(channel,
           options: CallOptions(
             metadata: {
-              "authorization": "Basic ${base64Encode(Utf8Encoder().convert(defaultServerKey))}"
+              "authorization":
+                  "Basic ${base64Encode(Utf8Encoder().convert(defaultServerKey))}"
             },
           ));
       await client.healthcheck(Empty());
+
+      var request = AuthenticateEmailRequest();
+      request.account = AccountEmail();
+      request.account.email = "tom@tom.com";
+      request.account.password = "aabbccddeeff";
+
+      try {
+        Session session = await client.authenticateEmail(request);
+        print("TOKEN:" + session.token);
+
+        var account = await client.getAccount(Empty(),
+            options: CallOptions(
+              metadata: {
+                "authorization": "Bearer ${session.token}",
+              },
+            ));
+        print("ACCOUNT:" + account.email);
+      } catch (e) {
+        print("err:" + e);
+      }
     });
   });
 }
